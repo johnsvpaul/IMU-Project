a921e085988c51f3482ca38173d2a52dd7def315
johns <johnspaul99@gmail.com>
2021-10-05 00:41:00 +0200

    removed unneeded files

--- compressor5.py
+++
@@ -1,8 +0,0 @@
-import lzhw
-import pandas as pd
-from sys import getsizeof
-
-gc = pd.read_csv("test.csv")
-
-chunks = gc.shape[0] / 4 ## to have 4 chunks
-compressed_chunks = lzhw.CompressedFromCSV("test.csv", chunksize = chunks)
--- timeseries.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-
-data = pd.read_csv('data.csv',sep=r'\s*,\s*')
-data.to_csv('dfsavename.csv.gz', compression='gzip')
